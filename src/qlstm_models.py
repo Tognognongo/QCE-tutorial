b70edd207045769a741878a9f952e2499ab17dfb
Tognognongo <78177552+Tognognongo@users.noreply.github.com>
2023-09-18 22:54:54 +0000

    Added QLSTMTagger class

--- src/qlstm_models.py
+++ src/qlstm_models.py
@@ -3,6 +3,35 @@ import torch.nn.functional as F
 
 from qlstm import QLSTM
 
+class QLSTMTagger(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size,
+                 n_qubits=0, backend='default.qubit'):
+        super(QLSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs,
+        # and outputs hidden states with dimensionality hidden_dim.
+
+        if n_qubits > 0:
+            #print(f'Tagger will use Quantum LSTM running on backend {backend}')
+            self.lstm = QLSTM(embedding_dim, hidden_dim, n_qubits=n_qubits, backend=backend)
+        else:
+            #print('Tagger will use Classical LSTM')
+            self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+
+    def forward(self, sentence):
+        embeds = self.word_embeddings(sentence)
+        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
+        tag_logits = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        tag_scores = F.log_softmax(tag_logits, dim=1)
+        
+        return tag_scores
+
 class QLSTMClassifier(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size,
                  n_qubits=0, n_qlayers=1, ising=False, probs=False, backend='default.qubit'):
