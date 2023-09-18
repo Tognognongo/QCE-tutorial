0d3bc57256ffbb317162fe849773ac46b3c1dead
Coello Perez <uvc@mac130081.ornl.gov>
2023-09-18 16:53:56 -0400

    Added libraries to be installed

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda env remove -n qnlp-tut
-conda env create -f environment.yml
-source activate qnlp-tut
-conda install -c conda-forge mpi4py openmpi
-python3 -m ipykernel install --user --name=qnlp-tut
